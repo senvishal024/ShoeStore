d0e2952abdc4b6f7dee7a0fef7a2fb3e16a9922b
senvishal024 <senvishal024@gmail.com>
2026-07-12 10:37:36 +0530

    Updated

--- frontend/src/Components/ProductEdit.jsx
+++ frontend/src/Components/ProductEdit.jsx
@@ -54,7 +54,7 @@ function ProductEdit() {
           type="text"
           placeholder="Enter Product Name"
           className="w-full border rounded-lg p-3 outline-none focus:ring-2 focus:ring-blue-500"
-          value={product.name||""}
+          value={product.name}
           onChange={(e)=>setProduct({
             ...product,
             name:e.target.value
