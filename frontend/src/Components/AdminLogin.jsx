0709c4cc9d8d6741e0888c81a3d5f78c32259ba6
senvishal024 <senvishal024@gmail.com>
2026-07-16 14:24:24 +0530

    updated login

--- frontend/src/Components/AdminLogin.jsx
+++ frontend/src/Components/AdminLogin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { FaArrowLeft } from 'react-icons/fa';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function AdminLogin() {
     const navigate=useNavigate();
