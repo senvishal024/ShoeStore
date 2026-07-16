696faaa3a1e55f99ee08b2c1d8928c8739aceb18
senvishal024 <senvishal024@gmail.com>
2026-07-16 20:17:31 +0530

    updated

--- backend/Middleware/adminMiddleware.js
+++ backend/Middleware/adminMiddleware.js
@@ -1,10 +1,12 @@
 const adminIsLoggedIn=(req,res,next)=>{
+    console.log("REQ USER:", req.user);
      if(!req.user){
 
         return res.status(401).json({
             message:"Login Required"
         });
     }
+    console.log("ROLE:", req.user.role);
     console.log("ADMIN",req.user)
     if(req.user.role !== "admin"){
         return res.status(403).json({
